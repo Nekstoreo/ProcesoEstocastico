103adebe41d19224f4f8d409544ea7ad67f6dc5d
Nestor Gutierrez <nestorg456k@outlook.com>
2023-10-19 17:52:46 -0500

    Cambios

--- pruebas_teoricas.py
+++ pruebas_teoricas.py
@@ -1,12 +1,16 @@
 import Monopoly as mp
 import matplotlib.pyplot as plt
 import tkinter as tk
+from tkinter import Frame
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def castToProb (dicc, n_partidas):
+
+def castToProb(dicc, n_partidas):
     for key in dicc:
-        if dicc[key] != 0:    
+        if dicc[key] != 0:
             dicc[key] = dicc[key] / n_partidas
     return dicc
+
 
 def simular_partidas(n_partidas):
     tablero = mp.Tablero()
@@ -19,8 +23,9 @@ def simular_partidas(n_partidas):
         jugador.mover()
         casilla_actual = jugador.posicion_actual.nombre
         casillas_visitadas[casilla_actual] += 1
-    
+
     return castToProb(casillas_visitadas, n_partidas)
+
 
 def obtener_values_casillas_visitadas(casillas_visitadas):
     values_casillas_visitadas = list(casillas_visitadas.values())
@@ -32,38 +37,82 @@ def cutDicc(dicc, n):
     for i in range(n):
         result[list(dicc.keys())[i]] = list(dicc.values())[i]
     return result
-    
 
 
 def graficar_casillas_visitadas(casillas_visitadas):
     nombres_casillas = list(casillas_visitadas.keys())
     valores_casillas = list(casillas_visitadas.values())
-    
-    plt.figure(figsize=(10, 5))
-    plt.bar(nombres_casillas, valores_casillas)
-    plt.xlabel('Casillas')
-    plt.ylabel('Visitas')
-    plt.title('Visitas a las casillas en el Monopoly')
-    plt.xticks(rotation=90)
-    plt.show()
-    
-def graficar_prob_dados(prob_dados):
+    # Crear el frame para el gráfico
+    frame = tk.Frame(window)
+    frame.pack()
+    # Limpiar el área del gráfico anterior (si existe)
+    for widget in frame.winfo_children():
+        widget.destroy()
+    # Crear el gráfico
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.bar(nombres_casillas, valores_casillas)
+    ax.set_xlabel("Casillas del Monopoly")
+    ax.set_ylabel("Frecuencia")
+    ax.set_title("Visitas a las casillas en el Monopoly")
+    ax.set_xticklabels(nombres_casillas, rotation=45)
+    ax.set_yticklabels(valores_casillas)
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.get_tk_widget().pack()
+    canvas.draw()
+
+
+def graficar_prob_dados():
+    markov = mp.Markov()
+    prob_dados = {i: markov.prob_dados[i] for i in range(13)}
     valor_dados = list(prob_dados.keys())
     prob_dados = list(prob_dados.values())
-    plt.figure(figsize=(10, 5))
-    plt.bar(valor_dados, prob_dados)
-    plt.xlabel('Suma de los dados')
-    plt.ylabel('Probabilidad')
-    plt.title('Probabilidad de la suma de los dados')
-    plt.xticks(rotation=90)
-    plt.show()
+    # Crear el frame para el gráfico
+    frame = tk.Frame(window)
+    frame.pack()
+    # Limpiar el área del gráfico anterior (si existe)
+    for widget in frame.winfo_children():
+        widget.destroy()
+    # Crear el gráfico
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.bar(valor_dados, prob_dados)
+    ax.set_xlabel("Valor de los dados")
+    ax.set_ylabel("Probabilidad")
+    ax.set_title("Probabilidad de los valores de los dados")
+    ax.set_xticklabels(valor_dados, rotation=90)
+    ax.set_yticklabels(prob_dados)
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.get_tk_widget().pack()
+    canvas.draw()
 
-markov = mp.Markov()
-prob_dados = {i: markov.prob_dados[i] for i in range(13)}
-    
+# def graficar_casillas_visitadas(casillas_visitadas):
+#    nombres_casillas = list(casillas_visitadas.keys())
+#    valores_casillas = list(casillas_visitadas.values())
+#
+#    plt.figure(figsize=(10, 5))
+#    plt.bar(nombres_casillas, valores_casillas)
+#    plt.xlabel('Casillas')
+#    plt.ylabel('Visitas')
+#    plt.title('Visitas a las casillas en el Monopoly')
+#    plt.xticks(rotation=90)
+#    plt.show()
+#
+# def graficar_prob_dados(prob_dados):
+#    valor_dados = list(prob_dados.keys())
+#    prob_dados = list(prob_dados.values())
+#    plt.figure(figsize=(10, 5))
+#    plt.bar(valor_dados, prob_dados)
+#    plt.xlabel('Suma de los dados')
+#    plt.ylabel('Probabilidad')
+#    plt.title('Probabilidad de la suma de los dados')
+#    plt.xticks(rotation=90)
+#    plt.show()
 
-n_partidas = 100000
-casillas_visitadas = cutDicc(simular_partidas(n_partidas), 12)
+
+def gengrafcasillas():
+    n_partidas = entry_n_partidas.get()
+    if n_partidas:  # Verificar que n_partidas no esté vacío
+        graficar_casillas_visitadas(
+            cutDicc(simular_partidas(int(n_partidas)), 12))
 
 
 # Create the Tkinter window
@@ -75,7 +124,11 @@ label_n_partidas.pack()
 entry_n_partidas = tk.Entry(window)
 entry_n_partidas.pack()
 # Create the button to generate the graph
-button_graficar = tk.Button(window, text="Generar gráfico", command=lambda: graficar_casillas_visitadas(cutDicc(simular_partidas(int(entry_n_partidas.get())), 13)))
-button_graficar.pack()
+button_casillas_visitadas = tk.Button(
+    window, text="Generar gráfico Casillas", command=gengrafcasillas)
+button_casillas_visitadas.pack()
+button_prob_dados = tk.Button(
+    window, text="Generar gráfico Dados", command=graficar_prob_dados)
+button_prob_dados.pack()
 # Run the Tkinter event loop
 window.mainloop()
